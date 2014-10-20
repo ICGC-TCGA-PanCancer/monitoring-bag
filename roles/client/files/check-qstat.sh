62cf8b6e664f1b95a47b90f541c043e3c4082639
George Mihaiescu <george.mihaiescu@oicr.on.ca>
2014-10-20 13:42:55 -0500

    Corrected the seqware user.

--- roles/client/files/check-qstat.sh
+++ roles/client/files/check-qstat.sh
@@ -2,7 +2,7 @@
 #
 # Verifies "qstat -f" output for errors
 
-out=`sudo -u seqware_user -i qstat -f | grep -i error| wc -l`
+out=`sudo -u seqware -i qstat -f | grep -i error| wc -l`
 
 if [ $out -gt 0 ]; then
   echo "Critical: $out jobs in error status!!"
--- roles/client/files/check-seqware-failed.sh
+++ roles/client/files/check-seqware-failed.sh
@@ -2,7 +2,7 @@
 #
 # It looks for failed seqware jobs
 
-out=`sudo -u seqware_user -i /home/seqware/bin/seqware workflow report --accession 2 | grep failed | wc -l`
+out=`sudo -u seqware -i /home/seqware/bin/seqware workflow report --accession 2 | grep failed | wc -l`
 
 if [ $out -gt 0 ]; then
   echo "Critical: $out seqware jobs have failed!!"
