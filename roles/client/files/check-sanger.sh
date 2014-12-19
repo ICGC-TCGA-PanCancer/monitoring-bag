7326df469ae810b2b0301380b287c389da4fbf80
George Mihaiescu <george.mihaiescu@oicr.on.ca>
2014-12-19 10:43:38 -0600

    Adding a simple check that looks in oozie for running jobs.

---
+++ roles/client/files/check-sanger.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+out=`oozie jobs -oozie http://localhost:11000/oozie | grep RUNNING | wc -l`
+
+# Check passes, only one job is running.
+if [[ $out -eq 1 ]]; then
+        echo "Single job running."
+        exit 0
+fi
+
+
+# Check fails, no jobs are running
+if [[ $out -eq 0 ]]; then
+        echo "Critical: no jobs are running!"
+        exit 2
+fi
+
+# More than 1 job is running
+echo "Critical: more than one job is running!"
+exit 2
