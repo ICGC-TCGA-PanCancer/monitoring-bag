4c82947561449acb950ab7ec6f195e8b89753393
George Mihaiescu <george.mihaiescu@oicr.on.ca>
2015-01-08 08:56:40 -0600

    Small improvements.

--- roles/client/files/check-sge-running-steps.sh
+++ roles/client/files/check-sge-running-steps.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -x
 out=`oozie jobs -oozie http://localhost:11000/oozie | grep RUNNING | wc -l`
+
 if [[ $out -eq 1 ]]; then
 status=`sudo -u seqware -i qstat -f| grep -A1 main.q@master| tail -n 1|awk '{print $5}'`
 step_name=`sudo -u seqware -i qstat -f| grep -A1 main.q@master| tail -n 1|awk '{print $3}'`
@@ -10,14 +11,14 @@ if [ -z $status ]
         then
 		if [ $status_qw = "qw" ]
 			then 
-                	echo "There are no SGE jobs queued, but none are running"
-                exit 1
+                	echo "There is at least one SGE job queued, but none are running"
+                exit 2
 		fi
 
 elif [ $status != "r" ]
         then
-                echo "The most recent SGE job is $step_name. but is queued instead of running."
-		exit 1
+                echo "The most recent SGE job is $step_name, but it is queued instead of running."
+		exit 2
 else
                 echo "Current workflow step is $step_name."
                 exit 0
