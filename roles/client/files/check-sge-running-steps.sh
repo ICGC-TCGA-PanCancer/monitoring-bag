d2134a401bb6a871612cfa77f008eb6fdba6b511
Ubuntu <ubuntu@bindle-sanger.(none)>
2015-01-21 13:18:19 +0000

    Added improved Sanger checks that show the last workflow status as well as slow downloads or uploads.

--- roles/client/files/check-sge-running-steps.sh
+++ roles/client/files/check-sge-running-steps.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
-set -x
+#set -x
 out=`oozie jobs -oozie http://localhost:11000/oozie | grep RUNNING | wc -l`
-
 if [[ $out -eq 1 ]]; then
 status=`sudo -u seqware -i qstat -f| grep -A1 main.q@master| tail -n 1|awk '{print $5}'`
 step_name=`sudo -u seqware -i qstat -f| grep -A1 main.q@master| tail -n 1|awk '{print $3}'`
-status_qw=`sudo -u seqware -i qstat -f | grep  -A2 "PENDING JOBS"| tail -n 1|awk '{print $5}'`
 
 if [ -z $status ]
         then
-		if [ $status_qw = "qw" ]
-			then 
-                	echo "There is at least one SGE job queued, but none are running"
-                exit 2
-		fi
+                echo "There are no SGE jobs/steps running, but Oozie reports a running workflow!"
+                exit 1
 
 elif [ $status != "r" ]
         then
-                echo "The most recent SGE job is $step_name, but it is queued instead of running."
-		exit 2
+                echo "The most recent SGE job is $step_name. but is queued instead of running."
+		exit 1
 else
                 echo "Current workflow step is $step_name."
                 exit 0
 fi
 fi
+
